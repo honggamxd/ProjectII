1af8398db2fab7812f0fe8cb6b6c9de41471dec9
Hồng Nguyễn Thị <32351149+honggamxd@users.noreply.github.com>
2019-05-31 16:11:51 +0700

    Add files via upload

---
+++ trangchu.php
@@ -0,0 +1,321 @@
+<?php include ('Server.php'); ?>
+<?php
+    $ms = $_SESSION['ms'];
+    $user = getUserbyId($ms);
+    if(isset($_GET['search'])){
+        $key = isset($_GET['keyword']) ? $_GET['keyword'] : null;
+        header('Location: search.php?search='.$key);}
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>QLCSVC - Trang chủ</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="trangchu.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          
+        </div>
+        <div class="sidebar-brand-text mx-3"> SOICT <br>  <sup>Quản lí CSVC/TB</sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+      <a class="nav-link collapsed" href="xuatdulieu.php" data-toggle="collapse" data-target="#collapsePages1" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Trích xuất dữ liệu</span></a>
+          <div id="collapsePages1" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="xuatdl-pb.php">Dữ liệu phòng ban</a>
+            <a class="collapse-item" href="xuatdl-tb.php">Dữ liệu các thiết bị</a>
+              <a class="collapse-item" href="dexuatthanhly.php">Đề xuất thanh lý</a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Divider -->
+      
+
+      <!-- Heading -->
+      
+
+      <!-- Nav Item - Pages Collapse Menu -->
+      <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>QL Thiết bị</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="themthietbi.php">Thêm thiết bị</a>
+            <a class="collapse-item" href="thongketb.php">Thống kê</a>
+          </div>
+        </div>
+      </li>
+      <hr class="sidebar-divider my-0">
+      <!-- Nav Item - Utilities Collapse Menu -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>QL Phòng ban</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+           
+            <a class="collapse-item" href="thongtinpb.php">Thông tin</a>
+            <a class="collapse-item" href="capnhatpb.php">Cập nhật dữ liệu</a>
+            
+          </div>
+        </div>
+      </li>
+<hr class="sidebar-divider my-0">
+
+      <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+            <a class="nav-link" href="kiemke.php">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Kiểm kê</span></a>
+        </li>
+
+        <hr class="sidebar-divider my-0">
+        <li class="nav-item">
+            <a class="nav-link" href="quanly.php">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Quản lý Nhân viên</span></a>
+        </li>
+        <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+          <!-- Topbar Search -->
+          <form method="get" action="" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" name="keyword" class="form-control bg-light border-0 small" placeholder="Tìm kiếm phòng/thiết bị" aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" name="search">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" name="keyword" class="form-control bg-light border-0 small" placeholder="Tìm kiếm phòng..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="submit" name="search">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+
+            <!-- Nav Item - Alerts -->
+            
+                
+              <!-- Dropdown - Alerts -->
+              
+
+            <!-- Nav Item - Messages -->
+            
+              <!-- Dropdown - Messages -->
+              
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $user['hoten']; ?></span>
+                <img class="img-profile rounded-circle" src="img/Capturec.JPG">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#"  data-toggle="modal" data-target="#logoutModal1">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Thông tin
+                </a>
+                
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Đăng xuất
+                </a>
+              </div>
+            </li>
+
+          </ul>
+
+        </nav>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <h1 class="h3 mb-2 text-gray-800">Hệ thống Cơ sở vật chất - thiết bị Viện Công nghệ thông tin và Truyền thông</h1>
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <p class="mb-4">Sinh viên được trải nghiệm trong môi trường thân thiện, hệ thống cơ sở vật chất hiện đại, đáp ứng nhu cầu học tập, nghiên cứu, tham gia các hoạt động thể thao, giải trí đa dạng.</p>
+                        <p class="mb-4">Khu vực Văn phòng Viện, Văn phòng các Bộ môn, các Phòng thí nghiệm phục vụ đào tạo, các Phòng thí nghiệm nghiên cứu được bố trí tại Tòa nhà B1. Hiện nay, Viện đang xây dựng  thêm 5 Phòng thí nghiệm nghiên cứu chuyên sâu.
+                        </p>
+                        <p class="mb-4">Phòng thí nghiệm Hệ thống máy tính, đặt tại P505 Tòa nhà B1, được đầu tư với nhiều trang thiết bị hiện đại phục vụ nghiên cứu thiết kế máy tính hiệu năng cao, máy tính nhúng phục vụ cho các ứng dụng trong nhiều lĩnh vực khác nhau, các hệ thống xử lý tín hiệu và xử lý tiếng Việt, …
+                            Trung tâm máy tính được bố trí tại tầng 3 nhà D5, với khoảng 200 máy tính, là nơi đào tạo thực hành Tin học đại cương cho sinh viên toàn trường và một số môn học về Công nghệ thông tin.</p>
+                        <p class="mb-4">Dự án Đào tạo về Công nghệ thông tin và Truyền thông Việt-Nhật được đặt tại tầng 8 tòa nhà Thư viện Tạ Quang Bửu với hệ thống trang thiết bị hiện đại đạt tiêu chuẩn cao, bao gồm:Khu vực Văn phòng; Các phòng làm việc của giáo viên tiếng Nhật; 04 phòng thực hành máy tính với 164 máy tính; 01 phòng thực hành mạch logic với các thiết bị thực hành như các bảng mạch, máy hiện sóng, máy phát tín hiệu, bộ nguồn một chiều; Thư viện; Phòng hội thảo.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+
+
+
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Thoát?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Huỷ</button>
+          <a class="btn btn-primary" href="login.php">Đăng xuất</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal fade" id="logoutModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Họ tên : <?php echo $user['hoten'] ?></h5>
+              </div>
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Chức vụ : <?php echo $user['chucvu'] ?></h5>
+              </div>
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Đơn vị : <?php echo $user['DV'] ?></h5>
+              </div>
+                  <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                  </button>
+              </div>
+
+          </div>
+      </div>
+  </div>
+
+
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
